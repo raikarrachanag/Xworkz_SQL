6dbfd3667f9772fbbcb8ea8da46d9161e560021d
unknown <rachanagraikar>
2023-09-12 11:04:59 +0530

    Unique

--- Sep_4th (insert, alter,Not null, Unique, PK and FK).sql
+++ Sep_4th (insert, alter,Not null, Unique, PK and FK).sql
@@ -310,3 +310,26 @@ INSERT INTO ORDERITEMS values(10009,105,24,20,'Kerala');
 INSERT INTO ORDERITEMS values(10010,109,25,14,'Chennai');
 
 select * from orderitems;
+
+CREATE TABLE Interview (InterviewID INT PRIMARY KEY,InterviewDate DATE,InterviewTime TIME,InterviewLocation VARCHAR(100),InterviewerID INT,CandidateID INT,InterviewType VARCHAR(50),InterviewRound INT,InterviewStatus VARCHAR(50),InterviewResult VARCHAR(50),InterviewFeedback TEXT,CandidateResume BLOB,InterviewerFeedback TEXT,TechnicalSkillsRating INT,CommunicationSkillsRating INT,ProblemSolvingSkillsRating INT,TeamworkSkillsRating INT,LeadershipSkillsRating INT,CulturalFitRating INT);
+select * from Interview;
+INSERT INTO Interview VALUES(1, '2023-09-10', '09:00:00', 'New York', 101, 201, 'Phone', 1, 'Scheduled', 'Pending', 'N/A', NULL, NULL, 4, 4, 3, 5, 4, 3);
+INSERT INTO Interview VALUES(2, '2023-09-10', '09:00:00', 'New York', 101, 201, 'Phone', 1, 'Scheduled', 'Pending', 'N/A', NULL, NULL, 4, 4, 3, 5, 4, 3);
+INSERT INTO Interview VALUES(3, '2023-09-10', '09:00:00', 'New York', 101, 201, 'Phone', 1, 'Scheduled', 'Pending', 'N/A', NULL, NULL, 4, 4, 3, 5, 4, 3);
+INSERT INTO Interview VALUES(4, '2023-09-13', '11:00:00', 'San Francisco', 104, 204, 'In-Person', 3, 'Completed', 'Accepted', 'Excellent performance', NULL, 'Impressive technical skills', 5, 4, 5, 5, 4, 4);
+INSERT INTO Interview VALUES(5, '2023-09-14', '09:30:00', 'Seattle', 105, 205, 'Video', 2, 'Completed', 'Rejected', 'Lacks experience', NULL, 'Average communication skills', 3, 3, 2, 4, 3, 2);
+INSERT INTO Interview VALUES(6, '2023-09-15', '13:45:00', 'Boston', 106, 206, 'In-Person', 1, 'Completed', 'Accepted', 'Great cultural fit', NULL, 'Strong problem-solving skills', 4, 4, 4, 5, 4, 5);
+INSERT INTO Interview VALUES(7, '2023-09-16', '15:20:00', 'Denver', 107, 207, 'Phone', 1, 'Scheduled', 'Pending', 'N/A', NULL, NULL, 4, 4, 3, 5, 4, 3);
+INSERT INTO Interview VALUES(8, '2023-09-17', '10:00:00', 'Austin', 108, 208, 'Video', 3, 'Completed', 'Accepted', 'Very professional', NULL, 'Excellent communication skills', 5, 5, 5, 4, 4, 5);
+INSERT INTO Interview VALUES(9, '2023-09-18', '11:30:00', 'Miami', 109, 209, 'In-Person', 2, 'Completed', 'Rejected', 'Insufficient technical knowledge', NULL, 'Needs improvement in teamwork', 3, 3, 2, 4, 3, 2);
+INSERT INTO Interview VALUES(10, '2023-09-19', '14:00:00', 'Houston', 110, 210, 'Phone', 1, 'Completed', 'Rejected', 'Lack of relevant experience', NULL, 'Weak problem-solving skills', 2, 3, 2, 3, 2, 3);
+INSERT INTO Interview VALUES(11, '2023-09-20', '10:15:00', 'Philadelphia', 111, 211, 'Video', 2, 'Completed', 'Accepted', 'Impressive skills', NULL, 'Excellent leadership abilities', 5, 4, 5, 5, 5, 4);
+INSERT INTO Interview VALUES(12, '2023-09-21', '16:30:00', 'Phoenix', 112, 212, 'In-Person', 4, 'Scheduled', 'Pending', 'N/A', NULL, NULL, 4, 4, 3, 5, 4, 3);
+INSERT INTO Interview VALUES(13, '2023-09-22', '09:45:00', 'Dallas', 113, 213, 'Phone', 1, 'Scheduled', 'Pending', 'N/A', NULL, NULL, 4, 4, 3, 5, 4, 3);
+INSERT INTO Interview VALUES(14, '2023-09-23', '11:20:00', 'San Diego', 114, 214, 'In-Person', 2, 'Completed', 'Accepted', 'Exceptional candidate', NULL, 'Outstanding problem-solving skills', 5, 5, 5, 5, 5, 5);
+INSERT INTO Interview VALUES(15, '2023-09-24', '15:00:00', 'Portland', 115, 215, 'Video', 1, 'Completed', 'Rejected', 'Lacks required certifications', NULL, 'Average teamwork skills', 3, 3, 3, 4, 3, 2);
+INSERT INTO Interview VALUES(16, '2023-09-25', '13:10:00', 'Atlanta', 116, 216, 'In-Person', 3, 'Completed', 'Accepted', 'Excellent presentation', NULL, 'Strong leadership qualities', 5, 5, 4, 4, 5, 4);
+INSERT INTO Interview VALUES(17, '2023-09-26', '14:45:00', 'Las Vegas', 117, 217, 'Phone', 1, 'Scheduled', 'Pending', 'N/A', NULL, NULL, 4, 4, 3, 5, 4, 3);
+INSERT INTO Interview VALUES(18, '2023-09-27', '10:50:00', 'Orlando', 118, 218, 'Video', 2, 'Completed', 'Rejected', 'Limited industry knowledge', NULL, 'Needs improvement in communication', 3, 3, 2, 4, 3, 2);
+INSERT INTO Interview VALUES(19, '2023-09-28', '09:20:00', 'Nashville', 119, 219, 'In-Person', 1, 'Completed', 'Accepted', 'Strong work ethic', NULL, 'Excellent problem-solving skills', 4, 4, 4, 5, 4, 5);
+INSERT INTO Interview VALUES(20, '2023-09-29', '12:00:00', 'Raleigh', 120, 220, 'Phone', 1, 'Completed', 'Rejected', 'Insufficient qualifications', NULL, 'Weak cultural fit', 2, 3, 2, 3, 2, 3);
